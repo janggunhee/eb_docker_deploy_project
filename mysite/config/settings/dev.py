05e35ea176434155527ef3983947d3d263230664
janggunhee <jan9.9unhee@gmail.com>
2017-11-27 16:42:21 +0900

    ALLOWED_HOSTS 수정

--- mysite/config/settings/dev.py
+++ mysite/config/settings/dev.py
@@ -1,11 +1,12 @@
 from .base import *
 config_secret = json.loads(open(CONFIG_SECRET_DEV_FILE).read())
+
 # AWS
-AWS_ACCESS_KEY_ID = config_secret_common['aws']['access_key_id']
-AWS_SECRET_ACCESS_KEY = config_secret_common['aws']['secret_access_key']
-AWS_STORAGE_BUCKET_NAME = config_secret_common['aws']['s3_bucket_name']
-AWS_S3_SIGNATURE_VERSION = 's3v4'
-AWS_S3_REGION_NAME = 'ap-northeast-2'
+AWS_ACCESS_KEY_ID = config_secret['aws']['access_key_id']
+AWS_SECRET_ACCESS_KEY = config_secret['aws']['secret_access_key']
+AWS_STORAGE_BUCKET_NAME = config_secret['aws']['s3_bucket_name']
+AWS_S3_SIGNATURE_VERSION = config_secret['aws']['s3_signature_version']
+AWS_S3_REGION_NAME = config_secret['aws']['s3_region_name']
 AWS_S3_HOST = 's3.ap-northeast-2.amazonaws.com'
 S3_USE_SIGV4 = True
 
@@ -20,3 +21,10 @@ STATICFILES_STORAGE = 'config.storages.StaticStorage'
 
 # Database
 DATABASES = config_secret['django']['databases']
+
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '.elasticbeanstalk.com',
+    '.janggunhee.com',
+]
