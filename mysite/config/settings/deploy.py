5e2e1f6e6e62137ce40256ec7aa0abb361a7d106
janggunhee <jan9.9unhee@gmail.com>
2017-11-27 16:28:07 +0900

    설정을 local폴더로 이동

---
+++ mysite/config/settings/deploy.py
@@ -0,0 +1,35 @@
+from pprint import pprint
+
+from .base import *
+config_secret = json.loads(open(CONFIG_SECRET_DEPLOY_FILE).read())
+DEBUG = False
+
+# AWS
+AWS_ACCESS_KEY_ID = config_secret['aws']['access_key_id']
+AWS_SECRET_ACCESS_KEY = config_secret['aws']['secret_access_key']
+AWS_STORAGE_BUCKET_NAME = config_secret['aws']['s3_bucket_name']
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_S3_REGION_NAME = 'ap-northeast-2'
+AWS_S3_HOST = 's3.ap-northeast-2.amazonaws.com'
+S3_USE_SIGV4 = True
+
+# AWS Storage
+STATICFILES_LOCATION = 'static'
+MEDIAFILES_LOCATION = 'media'
+
+# S3 FileStorage
+DEFAULT_FILE_STORAGE = 'config.storages.MediaStorage'
+STATICFILES_STORAGE = 'config.storages.StaticStorage'
+
+
+# Database
+DATABASES = config_secret['django']['databases']
+
+ALLOWED_HOSTS = [
+    # 'localhost',
+    # '127.0.0.1',
+    '.elasticbeanstalk.com',
+    '.janggunhee.com',
+]
+
+pprint(DATABASES)
